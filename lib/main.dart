d4026563e2ff48de92f7079e33804a6afaa591a9
Amrit Kumar <134717688+KumarAmrit30@users.noreply.github.com>
2025-01-13 21:26:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'ChatterBox',
+      title: 'Chatter',
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
